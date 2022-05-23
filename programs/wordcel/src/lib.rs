5943d8409650bece2f434e1137746b73c62a0f14
abishekk92 <bhat.abishek@gmail.com>
2022-05-23 17:45:40 +0530

    Migrate to latest anchor

--- programs/wordcel/src/lib.rs
+++ programs/wordcel/src/lib.rs
@@ -1,57 +1,59 @@
 use anchor_lang::prelude::*;
 
+use std::mem::size_of;
+
 declare_id!("v4enuof3drNvU2Y3b5m7K62hMq3QUP6qQSV2jjxAhkp");
+
+const MAX_LEN_URI: usize = 128;
 
 #[program]
 pub mod wordcel {
     use super::*;
 
-    pub fn initialize(
-        ctx: Context<Initialize>,
-        publication_bump: u8,
-        _random_hash: [u8; 32],
-    ) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, random_hash: [u8; 32]) -> Result<()> {
         let publication = &mut ctx.accounts.publication;
-        publication.bump = publication_bump;
+        publication.random_hash = random_hash;
+        publication.bump = *ctx.bumps.get("publication").unwrap();
         publication.authority = *ctx.accounts.user.to_account_info().key;
         Ok(())
     }
 
     pub fn create_post(
         ctx: Context<CreatePost>,
-        post_bump: u8,
         metadata_uri: String,
-        _random_hash: [u8; 32],
-    ) -> ProgramResult {
+        random_hash: [u8; 32],
+    ) -> Result<()> {
+        if metadata_uri.len() > MAX_LEN_URI {
+            return Err(error!(PostError::URITooLarge));
+        }
+
         let post = &mut ctx.accounts.post;
-        post.bump = post_bump;
+        post.random_hash = random_hash;
+        post.bump = *ctx.bumps.get("post").unwrap();
         post.metadata_uri = metadata_uri;
         post.publication = *ctx.accounts.publication.to_account_info().key;
         Ok(())
     }
 
-    pub fn update_post(ctx: Context<UpdatePost>, metadata_uri: String) -> ProgramResult {
+    pub fn update_post(ctx: Context<UpdatePost>, metadata_uri: String) -> Result<()> {
+        if metadata_uri.len() > MAX_LEN_URI {
+            return Err(error!(PostError::URITooLarge));
+        }
         let post = &mut ctx.accounts.post;
         post.metadata_uri = metadata_uri;
         Ok(())
     }
 
-    pub fn initialize_subscriber(
-        ctx: Context<InitializeSubscriber>,
-        subscriber_bump: u8,
-    ) -> ProgramResult {
+    pub fn initialize_subscriber(ctx: Context<InitializeSubscriber>) -> Result<()> {
         let subscriber = &mut ctx.accounts.subscriber;
-        subscriber.bump = subscriber_bump;
+        subscriber.bump = *ctx.bumps.get("subscriber").unwrap();
         subscriber.authority = *ctx.accounts.user.to_account_info().key;
         Ok(())
     }
 
-    pub fn initialize_subscription(
-        ctx: Context<InitializeSubscription>,
-        subscription_bump: u8,
-    ) -> ProgramResult {
+    pub fn initialize_subscription(ctx: Context<InitializeSubscription>) -> Result<()> {
         let subscription = &mut ctx.accounts.subscription;
-        subscription.bump = subscription_bump;
+        subscription.bump = *ctx.bumps.get("subscription").unwrap();
         subscription.publication = *ctx.accounts.publication.to_account_info().key;
         subscription.subscriber = *ctx.accounts.subscriber.to_account_info().key;
         let subscriber = &mut ctx.accounts.subscriber;
@@ -59,104 +61,136 @@ pub mod wordcel {
         Ok(())
     }
 
-    pub fn cancel_subscription(_ctx: Context<CancelSubscription>) -> ProgramResult {
+    pub fn cancel_subscription(_ctx: Context<CancelSubscription>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(publication_bump: u8, random_hash: [u8;32])]
+#[instruction(random_hash: [u8;32])]
 pub struct Initialize<'info> {
-    #[account(init, seeds=[b"publication".as_ref(), &random_hash],bump=publication_bump, payer=user)]
-    pub publication: Account<'info, Publication>,
+    #[account(init, seeds=[b"publication".as_ref(), &random_hash], bump, payer=user, space=Publication::LEN)]
+    publication: Account<'info, Publication>,
     #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(post_bump: u8, metadata_uri: String, random_hash: [u8;32])]
+#[instruction(metadata_uri: String, random_hash: [u8;32])]
 pub struct CreatePost<'info> {
-    #[account(mut, has_one=authority)]
-    pub publication: Account<'info, Publication>,
-    #[account(init, seeds=[b"post".as_ref(), &random_hash], bump=post_bump, payer=authority, space=256)]
-    pub post: Account<'info, Post>,
-    pub authority: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    #[account(mut, has_one=authority, seeds=[b"publication".as_ref(), &publication.random_hash], bump=publication.bump)]
+    // Checks if a publication was supplied and if the publication authority is the signer
+    publication: Account<'info, Publication>,
+    #[account(init, seeds=[b"post".as_ref(), &random_hash], bump, payer=authority, space=Post::LEN)]
+    post: Account<'info, Post>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 #[instruction(metadata_uri: String)]
 pub struct UpdatePost<'info> {
-    #[account(mut, has_one=authority)]
-    pub publication: Account<'info, Publication>,
-    #[account(mut, has_one=publication)]
-    pub post: Account<'info, Post>,
-    pub authority: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    #[account(mut, has_one=authority, seeds=[b"publication".as_ref(), &publication.random_hash], bump=publication.bump)]
+    // Checks if the original publication was supplied and if the publication authority is the signer
+    // What happens if they change the publication to a publication owned by the user.
+    publication: Account<'info, Publication>,
+    #[account(mut, has_one=publication, seeds=[b"post".as_ref(), &post.random_hash], bump=post.bump,)]
+    // Checks if a post was supplied and it is part of the supplied publication.
+    // What happens if they replace the post account with another post in the same publication
+    post: Account<'info, Post>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(subscriber_bump: u8)]
 pub struct InitializeSubscriber<'info> {
-    #[account(init, seeds=[b"subscriber".as_ref(), user.key().as_ref()],bump=subscriber_bump, payer=user)]
-    pub subscriber: Account<'info, Subscriber>,
+    #[account(init, seeds=[b"subscriber".as_ref(), user.key().as_ref()],bump, payer=user, space = Subscriber::LEN)]
+    subscriber: Account<'info, Subscriber>,
 
     #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(subscription_bump: u8)]
 pub struct InitializeSubscription<'info> {
     #[account(mut, has_one=authority)]
-    pub subscriber: Account<'info, Subscriber>,
-    #[account(init, seeds=[b"subscription".as_ref(), subscriber.key().as_ref(), &[subscriber.subscription_nonce as u8].as_ref()], bump=subscription_bump, payer=authority)]
-    pub subscription: Account<'info, Subscription>,
-    pub publication: Account<'info, Publication>,
-    pub authority: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    subscriber: Account<'info, Subscriber>,
+    #[account(init, seeds=[b"subscription".as_ref(), subscriber.key().as_ref(), &[subscriber.subscription_nonce as u8].as_ref()], bump, payer=authority, space=Subscription::LEN)]
+    subscription: Account<'info, Subscription>,
+    publication: Account<'info, Publication>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction()]
 pub struct CancelSubscription<'info> {
     #[account(mut, has_one=authority)]
-    pub subscriber: Account<'info, Subscriber>,
+    subscriber: Account<'info, Subscriber>,
     #[account(mut, close=authority)]
-    pub subscription: Account<'info, Subscription>,
-    pub authority: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    subscription: Account<'info, Subscription>,
+    authority: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(Default)]
 pub struct Publication {
-    pub bump: u8,
-    pub authority: Pubkey,
+    authority: Pubkey,
+    bump: u8,
+    random_hash: [u8; 32],
+}
+
+impl Publication {
+    const LEN: usize = 8 + size_of::<Self>();
 }
 
 #[account]
 #[derive(Default)]
 pub struct Post {
-    pub bump: u8,
-    pub publication: Pubkey,
-    pub metadata_uri: String,
+    publication: Pubkey,
+    metadata_uri: String,
+    bump: u8,
+    random_hash: [u8; 32],
+}
+
+impl Post {
+    const LEN: usize = 8 // Account Discriminator
+        + 32 // publication
+        + 32 // random_has
+        + 1 // bump
+        + MAX_LEN_URI;
 }
 
 #[account]
 #[derive(Default)]
 pub struct Subscriber {
-    pub bump: u8,
-    pub authority: Pubkey,
-    pub subscription_nonce: u8,
+    authority: Pubkey,
+    subscription_nonce: u8,
+    bump: u8,
+}
+
+impl Subscriber {
+    const LEN: usize = 8 + size_of::<Self>();
 }
 
 #[account]
 #[derive(Default)]
 pub struct Subscription {
-    pub bump: u8,
-    pub publication: Pubkey,
-    pub subscriber: Pubkey,
+    publication: Pubkey,
+    subscriber: Pubkey,
+    bump: u8,
+}
+
+impl Subscription {
+    const LEN: usize = 8 + size_of::<Self>();
+}
+
+#[error_code]
+pub enum PostError {
+    URITooLarge,
 }
--- tests/wordcel.ts
+++ tests/wordcel.ts
@@ -9,42 +9,35 @@ const {SystemProgram} = anchor.web3;
 
 describe('wordcel', async () => {
 
-    // Configure the client to use the local cluster.
-    const provider = anchor.Provider.env()
-    anchor.setProvider(provider);
+    const provider = anchor.getProvider();
 
     const program = anchor.workspace.Wordcel as Program<Wordcel>;
-    const user = provider.wallet;
-    // const user = anchor.web3.Keypair.generate();
+    const user = provider.wallet.publicKey;
     const randomHash = randombytes(32);
-    const publicationSeeds = [Buffer.from("publication"), randomHash];
-    const [publicationAccount, publicationBump] = await anchor.web3.PublicKey.findProgramAddress(publicationSeeds, program.programId);
+    const publicationSeed = [Buffer.from("publication"), randomHash];
+    const [publicationAccount, _] = await anchor.web3.PublicKey.findProgramAddress(publicationSeed, program.programId);
     let oneTrueFan: PublicKey;
     let onePostAccount: PublicKey;
 
     it("should initialize", async () => {
-        await program.rpc.initialize(publicationBump, randomHash, {
-            accounts: {
-                publication: publicationAccount,
-                user: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId,
-            }
-        });
+        await program.methods.initialize(randomHash)
+            .accounts({publication: publicationAccount, user: user, systemProgram: SystemProgram.programId})
+            .rpc();
+        const data = await program.account.publication.fetch(publicationAccount);
+        expect(data.authority.toString()).to.equal(user.toString());
     });
 
     it("should create a new post", async () => {
         const randomHash = randombytes(32);
         const postSeeds = [Buffer.from("post"), randomHash];
-        const [postAccount, postBump] = await anchor.web3.PublicKey.findProgramAddress(postSeeds, program.programId);
+        const [postAccount, _] = await anchor.web3.PublicKey.findProgramAddress(postSeeds, program.programId);
         const metadataUri = "https://gist.githubusercontent.com/abishekk92/10593977/raw/589238c3d48e654347d6cbc1e29c1e10dadc7cea/monoid.md";
-        await program.rpc.createPost(postBump, metadataUri, randomHash, {
-            accounts: {
-                post: postAccount,
-                publication: publicationAccount,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId,
-            }
-        });
+        await program.methods.createPost(metadataUri, randomHash).accounts({
+            post: postAccount,
+            publication: publicationAccount,
+            authority: user,
+            systemProgram: SystemProgram.programId,
+        }).rpc();
         const post = await program.account.post.fetch(postAccount);
         expect(post.metadataUri).to.equal(metadataUri);
         onePostAccount = postAccount;
@@ -52,28 +45,24 @@ describe('wordcel', async () => {
 
     it("should update post", async () => {
         const metadataUri = "https://gist.githubusercontent.com/shekdev/10593977/raw/589238c3d48e654347d6cbc1e29c1e10dadc7cea/monoid.md";
-        await program.rpc.updatePost(metadataUri, {
-            accounts: {
-                post: onePostAccount,
-                publication: publicationAccount,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId,
-            }
-        });
+        await program.methods.updatePost(metadataUri).accounts({
+            post: onePostAccount,
+            publication: publicationAccount,
+            authority: user,
+            systemProgram: SystemProgram.programId,
+        }).rpc();
         const post = await program.account.post.fetch(onePostAccount);
         expect(post.metadataUri).to.equal(metadataUri);
     });
 
     it("should create a subscriber", async () => {
-        const subscriberSeeds = [Buffer.from("subscriber"), user.publicKey.toBuffer()];
-        const [subscriberAccount, subscriberBump] = await anchor.web3.PublicKey.findProgramAddress(subscriberSeeds, program.programId);
-        await program.rpc.initializeSubscriber(subscriberBump, {
-            accounts: {
-                subscriber: subscriberAccount,
-                user: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
+        const subscriberSeeds = [Buffer.from("subscriber"), user.toBuffer()];
+        const [subscriberAccount, _] = await anchor.web3.PublicKey.findProgramAddress(subscriberSeeds, program.programId);
+        await program.methods.initializeSubscriber().accounts({
+            subscriber: subscriberAccount,
+            user: user,
+            systemProgram: SystemProgram.programId
+        }).rpc();
         const subscriber = await program.account.subscriber.fetch(subscriberAccount);
         expect(subscriber.subscriptionNonce).to.equal(0)
         oneTrueFan = subscriberAccount;
@@ -83,16 +72,14 @@ describe('wordcel', async () => {
         expect(oneTrueFan).to.not.equal(undefined);
         const oneTrueFanData = await program.account.subscriber.fetch(oneTrueFan);
         const subscriptionSeeds = [Buffer.from("subscription"), oneTrueFan.toBuffer(), new anchor.BN(oneTrueFanData.subscriptionNonce).toArrayLike(Buffer)];
-        const [subscriptionAccount, subscriptionBump] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
-        await program.rpc.initializeSubscription(subscriptionBump, {
-            accounts: {
-                subscription: subscriptionAccount,
-                subscriber: oneTrueFan,
-                publication: publicationAccount,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
+        const [subscriptionAccount, _] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
+        await program.methods.initializeSubscription().accounts({
+            subscription: subscriptionAccount,
+            subscriber: oneTrueFan,
+            publication: publicationAccount,
+            authority: user,
+            systemProgram: SystemProgram.programId
+        }).rpc();
         const subscription = await program.account.subscription.fetch(subscriptionAccount);
         expect(subscription.publication.toString()).to.equal(publicationAccount.toString());
         const subscriber = await program.account.subscriber.fetch(oneTrueFan);
@@ -103,28 +90,25 @@ describe('wordcel', async () => {
         expect(oneTrueFan).to.not.equal(undefined);
         const oneTrueFanData = await program.account.subscriber.fetch(oneTrueFan);
         const subscriptionSeeds = [Buffer.from("subscription"), oneTrueFan.toBuffer(), new anchor.BN(oneTrueFanData.subscriptionNonce).toArrayLike(Buffer)];
-        const [subscriptionAccount, subscriptionBump] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
-        await program.rpc.initializeSubscription(subscriptionBump, {
-            accounts: {
-                subscription: subscriptionAccount,
-                subscriber: oneTrueFan,
-                publication: publicationAccount,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
-        await program.rpc.cancelSubscription({
-            accounts: {
-                subscription: subscriptionAccount,
-                subscriber: oneTrueFan,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
+        const [subscriptionAccount, _] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
+        await program.methods.initializeSubscription().accounts({
+            subscription: subscriptionAccount,
+            subscriber: oneTrueFan,
+            publication: publicationAccount,
+            authority: user,
+            systemProgram: SystemProgram.programId
+        }).rpc();
+        await program.methods.cancelSubscription().accounts({
+            subscription: subscriptionAccount,
+            subscriber: oneTrueFan,
+            authority: user,
+            systemProgram: SystemProgram.programId
+        }).rpc();
         try {
             await program.account.subscription.fetch(subscriptionAccount);
         } catch (error) {
             expect(error).to.be.an('error');
+            expect(error.toString()).to.contain('Error: Account does not exist');
         }
     });
 
@@ -132,57 +116,48 @@ describe('wordcel', async () => {
         expect(oneTrueFan).to.not.equal(undefined);
         const oneTrueFanData = await program.account.subscriber.fetch(oneTrueFan);
         const subscriptionSeeds = [Buffer.from("subscription"), oneTrueFan.toBuffer(), new anchor.BN(oneTrueFanData.subscriptionNonce).toArrayLike(Buffer)];
-        const [subscriptionAccount, subscriptionBump] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
-        await program.rpc.initializeSubscription(subscriptionBump, {
-            accounts: {
-                subscription: subscriptionAccount,
-                subscriber: oneTrueFan,
-                publication: publicationAccount,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
+        const [subscriptionAccount, _] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
+        await program.methods.initializeSubscription().accounts({
+            subscription: subscriptionAccount,
+            subscriber: oneTrueFan,
+            publication: publicationAccount,
+            authority: user,
+            systemProgram: SystemProgram.programId
+        }).rpc();
         const randomUser = anchor.web3.Keypair.generate();
         try {
-            await program.rpc.cancelSubscription({
-                accounts: {
-                    subscription: subscriptionAccount,
-                    subscriber: oneTrueFan,
-                    authority: randomUser.publicKey,
-                    systemProgram: SystemProgram.programId
-                }
-            });
+            await program.methods.cancelSubscription().accounts({
+                subscription: subscriptionAccount,
+                subscriber: oneTrueFan,
+                authority: randomUser.publicKey,
+                systemProgram: SystemProgram.programId
+            }).rpc();
         } catch (error) {
             expect(error).to.be.an('error');
+            expect(error.toString()).to.contain('Error: Signature verification failed');
         }
     });
 
-    it("should allow authorized unsubscription", async () => {
-        expect(oneTrueFan).to.not.equal(undefined);
-        const oneTrueFanData = await program.account.subscriber.fetch(oneTrueFan);
-        const subscriptionSeeds = [Buffer.from("subscription"), oneTrueFan.toBuffer(), new anchor.BN(oneTrueFanData.subscriptionNonce).toArrayLike(Buffer)];
-        const [subscriptionAccount, subscriptionBump] = await anchor.web3.PublicKey.findProgramAddress(subscriptionSeeds, program.programId);
-        await program.rpc.initializeSubscription(subscriptionBump, {
-            accounts: {
-                subscription: subscriptionAccount,
-                subscriber: oneTrueFan,
-                publication: publicationAccount,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
-        await program.rpc.cancelSubscription({
-            accounts: {
-                subscription: subscriptionAccount,
-                subscriber: oneTrueFan,
-                authority: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            }
-        });
+    it("should only allow the post to be edited with the original publication", async () => {
+        const randomHash = randombytes(32);
+        const publicationSeed = [Buffer.from("publication"), randomHash];
+        const [newPublicationAccount, _] = await anchor.web3.PublicKey.findProgramAddress(publicationSeed, program.programId);
+        await program.methods.initialize(randomHash)
+            .accounts({publication: newPublicationAccount, user: user, systemProgram: SystemProgram.programId})
+            .rpc();
+        const metadataUri = "https://gist.githubusercontent.com/shekdev/10593977/raw/589238c3d48e654347d6cbc1e29c1e10dadc7cea/monoid.md";
         try {
-            await program.account.subscription.fetch(subscriptionAccount);
+            await program.methods.updatePost(metadataUri).accounts({
+                post: onePostAccount,
+                publication: newPublicationAccount,
+                authority: user,
+                systemProgram: SystemProgram.programId,
+            }).rpc();
+
         } catch (error) {
-            expect(error.toString()).to.contain('Error: Account does not exist');
+            expect(error).to.be.an('error');
+            expect(error.toString()).to.contain('Error Code: ConstraintHasOne');
         }
     });
+
 });
