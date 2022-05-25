9a82392b54c17a504b3d0705607a9ee7e2e5ad95
abishekk92 <bhat.abishek@gmail.com>
2022-05-25 21:26:35 +0530

    Split to logical files

---
+++ programs/wordcel/src/instructions.rs
@@ -0,0 +1,84 @@
+use crate::*;
+
+#[derive(Accounts)]
+#[instruction(random_hash: [u8;32])]
+pub struct Initialize<'info> {
+    #[account(init, seeds=[b"profile".as_ref(), &random_hash], bump, payer=user, space=Profile::LEN)]
+    pub profile: Account<'info, Profile>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(metadata_uri: String, random_hash: [u8;32])]
+pub struct CreatePost<'info> {
+    #[account(mut, has_one=authority, seeds=[b"profile".as_ref(), &profile.random_hash], bump=profile.bump)]
+    // Checks if a profile was supplied and if the profile authority is the signer
+    pub profile: Account<'info, Profile>,
+    #[account(init, seeds=[b"post".as_ref(), &random_hash], bump, payer=authority, space=Post::LEN)]
+    pub post: Account<'info, Post>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(metadata_uri: String)]
+pub struct UpdatePost<'info> {
+    #[account(mut, has_one=authority, seeds=[b"profile".as_ref(), &profile.random_hash], bump=profile.bump)]
+    // Checks if the original profile was supplied and if the profile authority is the signer
+    pub profile: Account<'info, Profile>,
+    #[account(mut, has_one=profile, seeds=[b"post".as_ref(), &post.random_hash], bump=post.bump,)]
+    // Checks if a post was supplied and it is part of the supplied profile.
+    pub post: Account<'info, Post>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(metadata_uri: String, random_hash: [u8;32])]
+pub struct Comment<'info> {
+    #[account(mut, has_one=authority, seeds=[b"profile".as_ref(), &profile.random_hash], bump=profile.bump)]
+    // Checks if a profile was supplied and if the profile authority is the signer
+    pub profile: Account<'info, Profile>,
+    #[account(init, seeds=[b"post".as_ref(), &random_hash, reply_to.key().as_ref()], bump, payer=authority, space=Post::LEN)]
+    pub post: Account<'info, Post>,
+    pub reply_to: Account<'info, Post>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeSubscriber<'info> {
+    #[account(init, seeds=[b"subscriber".as_ref(), user.key().as_ref()],bump, payer=user, space = Subscriber::LEN)]
+    pub subscriber: Account<'info, Subscriber>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeSubscription<'info> {
+    #[account(mut, has_one=authority)]
+    pub subscriber: Account<'info, Subscriber>,
+    #[account(init, seeds=[b"subscription".as_ref(), subscriber.key().as_ref(), &[subscriber.subscription_nonce as u8].as_ref()], bump, payer=authority, space=Subscription::LEN)]
+    pub subscription: Account<'info, Subscription>,
+    pub profile: Account<'info, Profile>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CancelSubscription<'info> {
+    #[account(mut, has_one=authority)]
+    pub subscriber: Account<'info, Subscriber>,
+    #[account(mut, close=authority)]
+    pub subscription: Account<'info, Subscription>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
---
+++ programs/wordcel/src/state.rs
@@ -0,0 +1,64 @@
+use crate::*;
+use std::mem::size_of;
+
+pub const MAX_LEN_URI: usize = 128;
+
+#[account]
+#[derive(Default)]
+pub struct Profile {
+    pub authority: Pubkey,
+    pub bump: u8,
+    pub random_hash: [u8; 32],
+}
+
+impl Profile {
+    pub const LEN: usize = 8 + size_of::<Self>();
+}
+
+#[account]
+#[derive(Default)]
+pub struct Post {
+    pub profile: Pubkey,
+    pub metadata_uri: String,
+    pub bump: u8,
+    pub random_hash: [u8; 32],
+
+    // Namespace created per project.
+    // Potentially monetize the platform by creating artificial scarcity of namespaces.
+    // namespace: Pubkey,
+
+    //Comments are just replies
+    pub reply_to: Option<Pubkey>,
+
+    //reshares are retweets or just share
+    // Reply with reshare is qoute tweet
+    pub reshare: bool,
+}
+
+impl Post {
+    pub const LEN: usize = 8 + 32 + size_of::<Self>() + MAX_LEN_URI;
+}
+
+#[account]
+#[derive(Default)]
+pub struct Subscriber {
+    pub authority: Pubkey,
+    pub subscription_nonce: u8,
+    pub bump: u8,
+}
+
+impl Subscriber {
+    pub const LEN: usize = 8 + size_of::<Self>();
+}
+
+#[account]
+#[derive(Default)]
+pub struct Subscription {
+    pub profile: Pubkey,
+    pub subscriber: Pubkey,
+    pub bump: u8,
+}
+
+impl Subscription {
+    pub const LEN: usize = 8 + size_of::<Self>();
+}
